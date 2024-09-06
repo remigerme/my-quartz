08e6d74963ddd1d982a1c250280b7305d6a72077
remigerme <remi.germe@gmail.com>
2024-09-07 00:37:55 +0200

    updated quartz config (url, name, traefik port)

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,12 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Raimmy's blog",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "blog.remigerme.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
