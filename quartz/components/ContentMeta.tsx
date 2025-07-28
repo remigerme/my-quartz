4af636a108c023c3c518215e1e901a49d7e2e831
remi.germe <remi.germe@gmail.com>
2025-07-27 20:22:35 -0700

    back using the comma

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -16,7 +16,7 @@ interface ContentMetaOptions {
 
 const defaultOptions: ContentMetaOptions = {
   showReadingTime: false,
-  showComma: false,
+  showComma: true,
 }
 
 export default ((opts?: Partial<ContentMetaOptions>) => {
