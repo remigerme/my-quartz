1efbb6053623bd128efa41d1273fe5d3b5a51de5
remi.germe <remi.germe@gmail.com>
2025-07-27 20:27:17 -0700

    try using cdnCaching: false bc fonts are not working on github pages (cors?)

--- quartz.config.ts
+++ quartz.config.ts
@@ -21,7 +21,7 @@ const config: QuartzConfig = {
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
-      cdnCaching: true,
+      cdnCaching: false,
       typography: {
         header: "Tinos",
         body: "Source Serif 4",
